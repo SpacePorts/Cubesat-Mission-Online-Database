0bc8785891d4002859cd93acf77eceb2507bcbb1
Administrator <m_pollen@yahoo.com>
2014-08-18 18:29:43 -0700

    slight fix to update script

--- Database/PropertyTable.php
+++ Database/PropertyTable.php
@@ -26,6 +26,61 @@ class PropertyTable extends Table
       array("column"=>new Column\Text("value")));
   }
 
+  public function GetProperty($property)
+  {
+    $lsql = new Sql($this->_adapter,"property");
+    $lselect = $lsql->Select();
+    $lselect->where(array("id"=>$property));
+    $lresults = $lsql->prepareStatementForSqlObject($lselect)->execute();
+    if($lresults == null)
+    {
+      return "";
+    }
+    else
+    {
+      $lresultSet = new ResultSet;
+      $lresultSet->initialize($lresults);
+
+      foreach ($lresultSet as $row) {
+        return $row["value"];
+      }
+    }
+  }
+
+  public function SetProperty($property,$value)
+  {
+    $lsql = new Sql($this->_adapter,"property");
+
+      $lselect = $lsql->Select();
+      $lselect->where(array("id"=>$value));
+
+      $lresults = $lsql->prepareStatementForSqlObject($lselect)->execute();
+
+      $lresultSet = new ResultSet;
+      $lresultSet->initialize($lresults);
+
+      $lvalueExist = false;
+      foreach ($lresultSet as $row) {
+         $lvalueExist = true;
+      }
+
+      if($lvalueExist == false)
+      {
+        $lsql = new Sql($this->_adapter,"property");
+        $linsert = $lsql->Insert();
+        $linsert->values(array("id"=>$property,"value"=>$value));
+        $lsql->prepareStatementForSqlObject($linsert)->execute();
+
+      }
+
+
+      $lsql = new Sql($this->_adapter,"property");
+      $lupdate =  $lsql->update();
+      $lupdate->Where(array("id"=>$property));
+      $lupdate->set(array("value"=>$value));
+      $lresults = $lsql->prepareStatementForSqlObject($lupdate)->execute();
+
+  }
 
 }
 
